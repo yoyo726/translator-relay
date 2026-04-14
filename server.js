8e00db6986597b552259d697dee0bde99bf1519b
yoyo726 <2714260024@qq.com>
2026-04-14 20:36:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import fetch from "node-fetch";
+const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json({ limit: "20mb" }));
